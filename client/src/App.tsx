8800d9a64b72a73b080cc78d65d702efd04864c2
hilma <shiral@hilma.tech>
2024-07-31 08:16:08 +0300

    feat(client): create client with input and select to the products and cateogires

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import BasicSelect from './components/BasicSelect'
+
+
+function App() {
+
+  return (
+    <>
+      <BasicSelect />
+    </>
+  )
+}
+
+export default App
